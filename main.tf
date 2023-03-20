c5ef4831a6c6320c005cdd205932807123f1f25e
anmol310792 <38555380+anmol310792@users.noreply.github.com>
2023-03-20 14:40:22 +0530

    error correction

--- main.tf
+++ main.tf
@@ -47,10 +47,10 @@ module "blog_sg" {
   version                  = "4.17.1"
   name                     = "blog"
 
-  vpc_id                   = module.vpc.public_subnets[0]
+  vpc_id                   = module.vpc.vpc_id
   ingress_rules            = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
 
   egress_rules             = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"] 
+  egress_cidr_blocks       = ["0.0.0.0/0"] 
 }
