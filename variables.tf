10f2eed2b972bc2c1f8ad8e282796112dbf4dae3
anmol310792 <38555380+anmol310792@users.noreply.github.com>
2023-03-20 16:05:40 +0530

    variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,39 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami-filter" {
+  description = "name & owner for ami"
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+values "environment" {
+  description = "name the environment"
+  type = object ({
+    name =  string
+    network_prefix = string
+  })
+
+  default = {
+    name           = "dev"
+    network_prefix = "10.0
+  }
+}
+
+variable "min_size" {
+  description = "mim number of instance in asg"
+  default = 1
+}
+
+variable "max_size" {
+  description = "max number of instance in asg"
+  default = 2
+}
