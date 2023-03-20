8e8ea296c2f92711fe7fc1a9447aaf9565115047
anmol310792 <38555380+anmol310792@users.noreply.github.com>
2023-03-20 15:34:19 +0530

    autoscaling

--- main.tf
+++ main.tf
@@ -31,16 +31,19 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  subnet_id              = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "Terraform"
-  }
+  vpc_zone_identifier = module.vpc.public_subnets
+  target_group_arns   = module.blog-alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  image_id                 = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
 module "blog-alb" {
@@ -61,12 +64,6 @@ module "blog-alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
